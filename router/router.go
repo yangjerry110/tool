45ae42e9b4094469674d4be65067ffa82436f2e2
yangjie04 <yangjie04@qutoutiao.net>
2024-05-31 11:32:47 +0800

    feat add router Run

--- router/router.go
+++ router/router.go
@@ -2,12 +2,15 @@
  * @Author: Jerry.Yang
  * @Date: 2023-12-18 16:57:27
  * @LastEditors: Jerry.Yang
- * @LastEditTime: 2024-04-12 17:44:37
+ * @LastEditTime: 2024-05-31 11:32:09
  * @Description: router
  */
 package router
 
-import "github.com/yangjerry110/tool/internal/router"
+import (
+	"github.com/yangjerry110/tool/internal/conf"
+	"github.com/yangjerry110/tool/internal/router"
+)
 
 // Register
 //
@@ -16,4 +19,13 @@ import "github.com/yangjerry110/tool/internal/router"
 // Author Jerry.Yang
 func Register(routerName string, routerRegister router.Register) error {
 	return routerEnginee().Register(routerName, routerRegister)
+}
+
+// Run
+//
+// Run
+// Date 2024-05-31 11:31:45
+// Author Jerry.Yang
+func Run(conf conf.Conf) error {
+	return routerEnginee().Run(conf)
 }
